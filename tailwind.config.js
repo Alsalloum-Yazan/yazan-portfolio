5ca04913eb7c1fe5d06163e442bce028b6cffcd0
Yazan Alsalloum <42650505+Alsalloum-Yazan@users.noreply.github.com>
2026-04-05 23:29:53 +0200

    Create tailwind.config.js with custom dark theme configuration

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/**
+ * This is a Tailwind CSS configuration file.
+ */
+
+module.exports = {
+  darkMode: 'class',
+  theme: {
+    extend: {
+      colors: {
+        electricBlue: '#00D9FF',
+        neonPurple: '#B000FF',
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
